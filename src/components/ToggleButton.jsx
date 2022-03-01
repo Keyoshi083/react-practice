f70cc3bfbdd3b82260a384545ea852f99e4dfcc1
Keyoshi083 <gt.chicken1@gmail.com>
2022-03-01 11:50:33 +0900

    useEffect入門

--- src/components/ToggleButton.jsx
+++ src/components/ToggleButton.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const ToggleButton = () => {
   const [open, setOpen] = useState(false);
@@ -6,6 +6,18 @@ const ToggleButton = () => {
   function toggle() {
     setOpen((prevState) => !prevState);
   }
+
+  //useEffect学習
+  //useEffect内で関数をreturnすればクリーンアップ処理が走る
+  // クリーンアップ処理は再レンダリングされる前に処理される（初回レンダリング含む）
+  useEffect(() => {
+    if (open) {
+      console.log("Connection open");
+    }
+    return () => {
+      console.log("Clean UP effect");
+    };
+  }, [open]);
   return (
     <div>
       <button onClick={toggle}>{open ? "OPEN" : "CLOSE"}</button>
